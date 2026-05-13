5c6fd9a0b6e1ed5fe7fa9f11f8065faeb592b866
Noam <noamrohloff@gmail.com>
2026-05-13 21:31:42 +0200

    more initializing and std::endl

--- learncppComPrograms/HelloWorld.cpp
+++ learncppComPrograms/HelloWorld.cpp
@@ -1,13 +1,15 @@
+//g++ -o Helloworld.exe HelloWorld.cpp
 #include <iostream>
 
 int main(){
     int width {5};//direct-list-initialization (preferred)
     int height {};//value initialization because it will be replaced soon
+    [[maybe_unused]] int i {4};
 
     std::cout << "Hello World!\n";
-    std::cout << width << " cm, now gimme hight in cm: ";
+    std::cout << width << " cm wide, now gimme hight in cm: ";
     std::cin >> height;
-    std::cout << "\n" << "You get a " << width * height << "cm cube.";
+    std::cout << "\n" << "You get a " << width * height << "cm cube." << std::endl;
     
     return 0;
 }
