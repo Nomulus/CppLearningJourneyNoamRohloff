e1f6bf3b6631d0577fc4ef13d6ab4842aa9d963f
Noam <noamrohloff@gmail.com>
2026-05-13 21:23:56 +0200

    different Types of initialization

--- learncppComPrograms/HelloWorld.cpp
+++ learncppComPrograms/HelloWorld.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main(){
-    std::cout<< "Hello World!\n";
+    int width {5};//direct-list-initialization (preferred)
+    int height {};//value initialization because it will be replaced soon
+
+    std::cout << "Hello World!\n";
+    std::cout << width << " cm, now gimme hight in cm: ";
+    std::cin >> height;
+    std::cout << "\n" << "You get a " << width * height << "cm cube.";
+    
     return 0;
 }
