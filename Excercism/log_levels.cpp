e3a2212efdf275e7a252d7c1aed1cbfe40685d5c
Noam <noamrohloff@gmail.com>
2026-05-14 15:44:25 +0200

    log_levels excercise completed

---
+++ Excercism/log_levels.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+namespace log_line
+{
+    std::string message(std::string line)
+    {
+        // return the message
+        return line.substr(line.find(":") + 2); //+2 damit auch :] weg ist
+    }
+
+    std::string log_level(std::string line)
+    {
+        // return the log level
+        return line.substr(1, line.find(":") - 2); //-2 damit man nur das Wort hat
+    }
+
+    std::string reformat(std::string line)
+    {
+        // return the reformatted message
+        return message(line) + " (" + log_level(line) + ")";
+    }
+} // namespace log_line
