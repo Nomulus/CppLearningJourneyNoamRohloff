8b1ef6a0eb57d3a685f7de9a8712ca245e5c795d
Noam <noamrohloff@gmail.com>
2026-05-14 08:03:44 +0200

    cout, cin, endl

--- learncppComPrograms/HelloWorld.cpp
+++ learncppComPrograms/HelloWorld.cpp
@@ -4,12 +4,15 @@
 int main(){
     int width {5};//direct-list-initialization (preferred)
     int height {};//value initialization because it will be replaced soon
+    int futureextraction {};
     [[maybe_unused]] int i {4};
 
     std::cout << "Hello World!\n";
     std::cout << width << " cm wide, now gimme hight in cm: ";
     std::cin >> height;
-    std::cout << "\n" << "You get a " << width * height << "cm cube." << std::endl;
+    std::cout << " a random val: ";
+    std::cin >> futureextraction;
+    std::cout << "\n" << height << " cm high you get a " << width * height << " cm cube." << std::endl;
     
     return 0;
 }
