900f6902a124c43abce6e1575b91b9f03f937ee0
Noam <noamrohloff@gmail.com>
2026-05-15 20:33:57 +0200

    nice, reverse string done

--- Excercism/reverse_string.cpp
+++ Excercism/reverse_string.cpp
@@ -1,8 +1,14 @@
 #include "reverse_string.h"
+#include <algorithm>
+#include <string>
 
 namespace reverse_string
 {
 
     // TODO: add your solution here
-
+    std::string reverse_string(std::string input)
+    {
+        std::reverse(input.begin(), input.end());
+        return input;
+    }
 } // namespace reverse_string
--- Excercism/reverse_string.h
+++ Excercism/reverse_string.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <string>
 
 namespace reverse_string
 {
 
     // TODO: add your solution here
+    std::string reverse_string(std::string input);
 
 } // namespace reverse_string
