11be0f8bc9780dff3daae4e91e305b15c3abe241
Noam <noamrohloff@gmail.com>
2026-05-15 20:20:06 +0200

    done raindrops excercise

--- Excercism/raindrops.cpp
+++ Excercism/raindrops.cpp
@@ -1,8 +1,30 @@
 #include "raindrops.h"
+#include <string>
 
 namespace raindrops
 {
-
     // TODO: add your solution here
+    std::string convert(int number)
+    {
+        std::string sound{""};
+        if (number % 3 == 0)
+        {
+            sound = sound + "Pling";
+        }
+        if (number % 5 == 0)
+        {
+            sound = sound + "Plang";
+        }
+        if (number % 7 == 0)
+        {
+            sound = sound + "Plong";
+        }
 
+        if (sound == "")
+        {
+            sound = std::to_string(number);
+        }
+
+        return sound;
+    }
 } // namespace raindrops
