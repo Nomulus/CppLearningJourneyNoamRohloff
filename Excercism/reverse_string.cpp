79bc79b80e742c8b3105554b9ed0f3c4cd8e4dfc
Noam <noamrohloff@gmail.com>
2026-05-15 20:20:56 +0200

    new excercise, reverse string

---
+++ Excercism/reverse_string.cpp
@@ -0,0 +1,8 @@
+#include "reverse_string.h"
+
+namespace reverse_string
+{
+
+    // TODO: add your solution here
+
+} // namespace reverse_string
---
+++ Excercism/reverse_string.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace reverse_string
+{
+
+    // TODO: add your solution here
+
+} // namespace reverse_string
