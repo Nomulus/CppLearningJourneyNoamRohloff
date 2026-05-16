0b6b1eaf04996b35829e899d884ca287eaf39575
Noam <noamrohloff@gmail.com>
2026-05-16 11:18:12 +0200

    did esxcercise

--- Excercism/difference_of_squares.cpp
+++ Excercism/difference_of_squares.cpp
@@ -4,5 +4,34 @@ namespace difference_of_squares
 {
 
     // TODO: add your solution here
+    int square_of_sum(int number)
+    {
+        int square_sum{0};
 
-} // namespace difference_of_squares
+        for (int i{0}; i <= number; i++)
+        {
+            square_sum = square_sum + i;
+        }
+
+        square_sum = square_sum * square_sum;
+
+        return square_sum;
+    }
+
+    int sum_of_squares(int number)
+    {
+
+        int sum_square{0};
+
+        for (int i{0}; i <= number; i++)
+        {
+            sum_square = sum_square + i * i;
+        }
+
+        return sum_square;
+    }
+    int difference(int number)
+    {
+        return square_of_sum(number) - sum_of_squares(number);
+    }
+}
--- Excercism/difference_of_squares.h
+++ Excercism/difference_of_squares.h
@@ -4,5 +4,8 @@ namespace difference_of_squares
 {
 
     // TODO: add your solution here
+    int square_of_sum(int number);
+    int sum_of_squares(int number);
+    int difference(int number);
 
 } // namespace difference_of_squares
