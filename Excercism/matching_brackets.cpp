7efeb1ae0c6837591d55b98b8f8ca550707643be
Noam <noamrohloff@gmail.com>
2026-05-17 21:11:30 +0200

    in excercise upload

---
+++ Excercism/matching_brackets.cpp
@@ -0,0 +1,25 @@
+#include "matching_brackets.h"
+
+namespace matching_brackets
+{
+
+    // TODO: add your solution here
+    bool check(std::string bracket_test)
+    {
+        std::vector<char> brackets{};
+
+        for (char character : bracket_test)
+        {
+            if (character = '(')
+            {
+            }
+            if (character = '[')
+            {
+            }
+            if (character = '{')
+            {
+            }
+        }
+    }
+
+} // namespace matching_brackets
---
+++ Excercism/matching_brackets.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace matching_brackets
+{
+
+    // TODO: add your solution here
+    bool check(std::string bracket_test);
+
+} // namespace matching_brackets
