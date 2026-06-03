b8b636dfeaafe7b9b494f91c909d6c401d5f86eb
Nomulus <noamrohloff@gmail.com>
2026-06-03 21:03:17 +0200

    Basic window structure

--- Raylib Simulations/Pong.cpp
+++ Raylib Simulations/Pong.cpp
@@ -1,3 +1,24 @@
-/*.\raylib\w64devkit\bin\g++.exe first_simulation.cpp -I.\raylib\raylib\src
+/*.\raylib\w64devkit\bin\g++.exe Pong.cpp -I.\raylib\raylib\src
  -L.\raylib\raylib\src -lraylib -lopengl32 -lgdi32 -lwinmm -o programm
  .\programm.exe*/
+
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main() {
+  cout << "Start Programming the game now";
+
+  const int screen_width = 1200;
+  const int screen_height = 800;
+  InitWindow(GetScreenWidth(), screen_height, "PongByNoam");
+  SetTargetFPS(60);
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+
+    EndDrawing();
+  }
+
+  return 0;
+}
