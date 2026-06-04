3d33dfd13a98351f1b97c9925ef574c80e99b1d9
Nomulus <noamrohloff@gmail.com>
2026-06-04 21:12:24 +0200

    Added Player movement
    independant player movement so i can add upgrades down the line.

--- Raylib Simulations/Pong.cpp
+++ Raylib Simulations/Pong.cpp
@@ -1,6 +1,4 @@
-/*.\raylib\w64devkit\bin\g++.exe Pong.cpp -I.\raylib\raylib\src
- -L.\raylib\raylib\src -lraylib -lopengl32 -lgdi32 -lwinmm -o programm
- .\programm.exe*/
+/* g++ Pong.cpp -lraylib -o pong.exe*/
 
 #include <iostream>
 #include <raylib.h>
@@ -8,17 +6,21 @@
 using namespace std;
 
 int main() {
-  int deltaTime{};
+  float deltaTime{0.0f};
 
   Vector2 WindowSize = {1200, 800};
 
   const int circleRadius = 20;
   Vector2 ballPos = {WindowSize.x / 2, WindowSize.y / 2};
 
-  Vector2 startingPlayerSize = {20, 120};
-  Vector2 player1Pos = {0, WindowSize.y / 2.0f - startingPlayerSize.y / 2};
-  Vector2 player2Pos = {WindowSize.x - startingPlayerSize.x,
-                        WindowSize.y / 2.0f - startingPlayerSize.y / 2};
+  Vector2 player1Size{20, 120};
+  Vector2 player2Size{20, 120};
+  Vector2 player1Pos = {0, WindowSize.y / 2.0f - player1Size.y / 2};
+  Vector2 player2Pos = {WindowSize.x - player2Size.x,
+                        WindowSize.y / 2.0f - player2Size.y / 2};
+
+  int player1Speed = 3.0f;
+  int player2Speed = 3.0f;
 
   InitWindow(WindowSize.x, WindowSize.y, "PongByNoam");
   SetTargetFPS(144);
@@ -28,13 +30,27 @@ int main() {
 
     // ----------- updating ----------
 
-    // ----------- drawing ---------------
+    if (IsKeyDown(KEY_W) && (player1Pos.y > 0)) {
+      player1Pos.y -= player1Speed;
+    }
+    if (IsKeyDown(KEY_S) && (player1Pos.y < WindowSize.y - player1Size.y)) {
+      player1Pos.y += player1Speed;
+    }
 
+    if (IsKeyDown(KEY_UP) && (player2Pos.y > 0)) {
+      player2Pos.y -= player2Speed;
+    }
+    if (IsKeyDown(KEY_DOWN) && (player2Pos.y < WindowSize.y - player2Size.y)) {
+      player2Pos.y += player2Speed;
+    }
+    // ----------- drawing ---------------
     BeginDrawing();
 
+    ClearBackground(BLACK);
+
     DrawCircleV(ballPos, circleRadius, WHITE);
-    DrawRectangleV(player1Pos, startingPlayerSize, WHITE);
-    DrawRectangleV(player2Pos, startingPlayerSize, WHITE);
+    DrawRectangleV(player1Pos, player1Size, WHITE);
+    DrawRectangleV(player2Pos, player2Size, WHITE);
 
     EndDrawing();
   }
