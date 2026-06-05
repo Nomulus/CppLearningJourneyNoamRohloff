39429f68a387c53d7ce7682547f6f4508b79031f
Nomulus <noamrohloff@gmail.com>
2026-06-05 20:34:28 +0200

    ball bouncing off walls and calling Standard game

--- Raylib Simulations/Pong.cpp
+++ Raylib Simulations/Pong.cpp
@@ -7,56 +7,76 @@
 
 using namespace std;
 
-int main() {
-  float deltaTime{0.0f};
+int StandardLoop(Vector2 windowSize, int startingSide) {
 
-  Vector2 WindowSize = {1200, 800};
-
-  const int circleRadius = 20;
-  Vector2 ballPos = {WindowSize.x / 2, WindowSize.y / 2};
+  const int circleRadius = 13;
+  Vector2 ballPos = {windowSize.x / 2, windowSize.y / 2};
+  Vector2 newBallPos = {};
 
   random_device rd;
   mt19937 gen(rd());
-  uniform_int_distribution<int> distr(0, 300);
+  uniform_int_distribution<int> distrX(700, 900);
+  uniform_int_distribution<int> distrY(-300, 300);
 
-  Vector2 ballSpeed = {(float)distr(gen), (float)distr(gen)};
+  Vector2 ballSpeed = {(float)distrX(gen), (float)distrY(gen)};
 
   Vector2 player1Size{20, 120};
   Vector2 player2Size{20, 120};
-  Vector2 player1Pos = {0, WindowSize.y / 2.0f - player1Size.y / 2};
-  Vector2 player2Pos = {WindowSize.x - player2Size.x,
-                        WindowSize.y / 2.0f - player2Size.y / 2};
+  Vector2 player1Pos = {0, windowSize.y / 2.0f - player1Size.y / 2};
+  Vector2 player2Pos = {windowSize.x - player2Size.x,
+                        windowSize.y / 2.0f - player2Size.y / 2};
 
-  int player1Speed = 300.0f;
-  int player2Speed = 300.0f;
+  int player1Speed = 400.0f;
+  int player2Speed = 400.0f;
 
-  InitWindow(WindowSize.x, WindowSize.y, "PongByNoam");
+  InitWindow(windowSize.x, windowSize.y, "PongByNoam");
   SetTargetFPS(144);
+  double deltaTime{};
 
   while (WindowShouldClose() == false) {
     deltaTime = GetFrameTime();
 
     // ----------- updating ----------
 
-    ballPos = Vector2Add(ballPos, Vector2Scale(ballSpeed, (float)deltaTime));
+    newBallPos = Vector2Add(ballPos, Vector2Scale(ballSpeed, (float)deltaTime));
+    if (newBallPos.x < 0 + circleRadius) {
+      ballSpeed.x = -ballSpeed.x - 100;
+      newBallPos = ballPos;
+    }
+    if (newBallPos.x > windowSize.x - circleRadius) {
+      ballSpeed.x = -ballSpeed.x - 100;
+      newBallPos = ballPos;
+    }
+    if (newBallPos.y < 0 + circleRadius) {
+      ballSpeed.y = -ballSpeed.y - 100;
+      newBallPos = ballPos;
+    }
+    if (newBallPos.y > windowSize.y - circleRadius) {
+      ballSpeed.y = -ballSpeed.y - 1.0;
+      newBallPos = ballPos;
+    }
+    ballPos = newBallPos;
 
+    // adjust ballSpeed to speed hit by Player
+    //
     if (IsKeyDown(KEY_W) && (player1Pos.y > 0)) {
       player1Pos.y -= player1Speed * GetFrameTime();
     }
-    if (IsKeyDown(KEY_S) && (player1Pos.y < WindowSize.y - player1Size.y)) {
+    if (IsKeyDown(KEY_S) && (player1Pos.y < windowSize.y - player1Size.y)) {
       player1Pos.y += player1Speed * GetFrameTime();
     }
 
     if (IsKeyDown(KEY_UP) && (player2Pos.y > 0)) {
       player2Pos.y -= player2Speed * GetFrameTime();
     }
-    if (IsKeyDown(KEY_DOWN) && (player2Pos.y < WindowSize.y - player2Size.y)) {
+    if (IsKeyDown(KEY_DOWN) && (player2Pos.y < windowSize.y - player2Size.y)) {
       player2Pos.y += player2Speed * GetFrameTime();
     }
     // ----------- drawing ---------------
     BeginDrawing();
 
-    ClearBackground(BLACK);
+    ClearBackground(GRAY);
+    DrawRectangle(0, 0, windowSize.x, windowSize.y, BLACK);
 
     DrawCircleV(ballPos, circleRadius, WHITE);
     DrawRectangleV(player1Pos, player1Size, WHITE);
@@ -65,5 +85,13 @@ int main() {
     EndDrawing();
   }
 
+  return 0;
+}
+
+int main() {
+  float deltaTime{0.0f};
+
+  Vector2 windowSize = {1200, 800};
+  StandardLoop(windowSize);
   return 0;
 }
